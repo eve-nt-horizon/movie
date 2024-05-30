c09830caf8c3a21ed6101e6f37f36dd93eed9306
mikaflatah@gmail.com <Mika>
2024-05-30 14:23:11 +0530

    tmdb uri builder

--- lib/builders/https_uri_builder.dart
+++ lib/builders/https_uri_builder.dart
@@ -1,40 +1,49 @@
+import 'package:movie/builders/builder.dart';
 
 class HttpsUriBuilder extends Builder {
-  String authority = '';
-  String path = '';
-  Map<String, dynamic> queryParams = {};
+  String _authority = '';
+  String _path = '';
+  Map<String, dynamic> _queryParams = {};
   HttpsUriBuilder._();
   factory HttpsUriBuilder.create() {
     return HttpsUriBuilder._();
   }
-  void setAuthority(String authority) {
-    this.authority = authority;
+  HttpsUriBuilder setAuthority(String authority) {
+    this._authority = authority;
+    return this;
   }
 
-  void setPath(String path) {
-    this.path = path;
+  HttpsUriBuilder setPath(String path) {
+    this._path = path;
+    return this;
   }
 
-  void setQueryParams(Map<String, dynamic> queryParams) {
-    this.queryParams = queryParams;
+  HttpsUriBuilder setQueryParams(Map<String, dynamic> queryParams) {
+    this._queryParams = queryParams;
+    return this;
   }
 
   @override
   Uri build() {
-    Uri uri = Uri.https(authority, path, queryParams);
+    Uri uri = Uri.https(_authority, _path, _queryParams);
     reset();
     return uri;
   }
 
   @override
   void reset() {
-    authority = '';
-    path = '';
-    queryParams = {};
+    _authority = '';
+    _path = '';
+    _queryParams = {};
   }
 }
 
-abstract class Builder {
-  void build();
-  void reset();
+void main() {
+  var httpsBuilder = HttpsUriBuilder.create();
+  httpsBuilder.setAuthority('api.themoviedb.org');
+  httpsBuilder.setPath('/3/discover/movie');
+  Map<String, dynamic> queryParams = {'with_genres': '28'};
+  httpsBuilder.setQueryParams(queryParams);
+  Uri uri = httpsBuilder.build();
+  print(uri);
 }
